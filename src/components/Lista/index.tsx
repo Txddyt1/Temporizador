3b40f307852bfaa90b97d692dd5a8f9d1fac5d59
Txddyt1 <igorschuller40@gmail.com>
2025-08-18 15:06:07 -0300

    Feat: Selecionar item

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -2,15 +2,22 @@ import style from './Lista.module.scss'
 import Item from "./Item";
 import { ITarefa } from '../../types/tarefa';
 
+interface Props{
+    tarefas: ITarefa[];
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void;
+}
 
-
-function Lista({tarefas}: {tarefas: ITarefa[]}) {
+function Lista({tarefas, selecionaTarefa}: Props) {
     return (
         <aside className={style.listaTarefas}>
             <h2 >Estudos do dia</h2>
             <ul>
-                {tarefas.map((item, index) => (
-                    <Item key={index} {...item}/>
+                {tarefas.map(item => (
+                    <Item 
+                    selecionaTarefa={selecionaTarefa}
+                    key={item.id}
+                    {...item}
+                    />
                 ))}
             </ul>
         </aside>
